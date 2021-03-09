c2490a76e30850d9906bb64ad2c37f189cc1a81e
Dmitriy Erokhin <derohin@nn-centr.ru>
2021-03-09 13:48:25 +0300

    main

--- pgbackup.sh
+++ pgbackup.sh
@@ -3,22 +3,34 @@
 BASES=`sudo -u postgres psql -A -q -t -c "select datname from pg_database" template1`
 TIME=`date +"%d-%m-%y_%T"`
 DATE=`date +"%d-%m-%y"`
-BACKUPDIR="/home/backup/1CBackup"
-LOGDIR="/var/log/1CBackup"
+
+#Задаём параметры хранения
+KEEP=14 #Сколько дней храним бэкапы
+BACKUPDIR="/home/backup/1CBackup" #Пусть к папке с бэкапами
+LOGDIR="/var/log/1CBackup" #Путь к папке с логами
+
+#Задаём параметры аторизации
 #PGUSER=postgres
 #PGPASSWORD=pgpass
 PGPASSFILE=/root/.pgpass
 
+#Бэкапим все базы (включая служебные)
 for i in $BASES
 do
   if [ ! -d "$LOGDIR/$i" ]; then
     mkdir "$LOGDIR/$i"
   fi
-  echo "$TIME: Start backup of %i database" > $LOGDIR/$i/$TIME.log
+  TIME=`date +"%d-%m-%y_%T"`
+  echo "$TIME: Start backup of database" >> $LOGDIR/$i/$DATE.log
   if [ ! -d "$BACKUPDIR/$i" ]; then
     mkdir "$BACKUPDIR/$i"
   fi
 
   pg_dump $i -U dbadmin --format=custom --file=$BACKUPDIR/$i/$i_$TIME.backup
-  echo "$TIME: Finish backup of %i database" >> $LOGDIR/$i/$TIME.log
+  
+  TIME=`date +"%d-%m-%y_%T"`
+  echo "$TIME: Finish backup of database" >> $LOGDIR/$i/$DATE.log
 done
+
+#Удаляем старые бэкапы
+find $BACKUPDIR/* -name "*.backup" -atime +$KEEP | xargs rm -rf
